269227bb5f03bf4c00cb40f9d66029bb79171d2c
Thanuja Perera <it25101195@my.sliit.lk>
2026-05-13 14:49:46 +0530

    deleted

--- src/main/java/com/pageturner/repository/NotificationRepository.java
+++ src/main/java/com/pageturner/repository/NotificationRepository.java
@@ -1,6 +1,5 @@
 package com.pageturner.repository;
 
-import com.pageturner.model.Notification;
 import com.pageturner.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
