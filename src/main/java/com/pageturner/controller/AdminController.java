d13c221eed08a5b21f3f5684e2e2a77eb00619e6
Thanuja Perera <it25101195@my.sliit.lk>
2026-05-17 19:26:00 +0530

    ModelView

--- src/main/java/com/pageturner/controller/AdminController.java
+++ src/main/java/com/pageturner/controller/AdminController.java
@@ -8,6 +8,7 @@ import com.pageturner.service.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.math.BigDecimal;
@@ -119,20 +120,21 @@ public class AdminController {
     // --- Author Management ---
 
     @GetMapping("/authors")
-    public String listAuthors(Model model, RedirectAttributes redirectAttributes) {
+    public ModelAndView listAuthors(Model model, RedirectAttributes redirectAttributes) {
         try {
+            ModelAndView modelAndView = new ModelAndView("admin/authors/list");
             model.addAttribute("authors", authorService.getAllAuthors());
-            return "admin/authors/list";
+            return modelAndView;
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("error", "Could not load authors: " + e.getMessage());
-            return "redirect:/admin";
+            return new ModelAndView("index");
         }
     }
 
     @GetMapping("/authors/add")
-    public String showAddAuthorForm(Model model) {
+    public ModelAndView showAddAuthorForm(Model model) {
         model.addAttribute("author", new Author());
-        return "admin/authors/form";
+        return new ModelAndView("admin/authors/form");
     }
 
     @PostMapping("/authors/save")
